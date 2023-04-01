b93f3d419d5732118efc64223e2ac833a9019952
ariollex <ariollex@gmail.com>
2023-04-01 16:38:56 +0300

    Updater: add ability to generate .dmg file
    dmg is needed to properly install the program on macOS.

--- build.py
+++ build.py
@@ -2,25 +2,42 @@
 import PyInstaller.__main__
 import pyinstaller_versionfile
 from main import version
+import platform
 import shutil
 import os
+
+# Variables
+app_name = "Updater"
+app_description = "This is a simple Updater for Python programs."
 
 # Removing dist
 if os.path.exists('dist'):
     shutil.rmtree('dist')
 
-# Make version file
-pyinstaller_versionfile.create_versionfile(
-    output_file="version_file.txt",
-    version=version,
-    file_description="This is a simple Updater for Python programs.",
-    internal_name="Updater",
-    original_filename="Updater.exe",
-    product_name="Updater",
-    translations=[1033, 1252, 1251]
-)
+if platform.system() == 'Windows':
+    # Make version file for exe
+    pyinstaller_versionfile.create_versionfile(
+        output_file="build_configuration/version_file.txt",
+        version=version,
+        file_description=app_description,
+        internal_name=app_name,
+        original_filename=app_name + ".exe",
+        product_name=app_name,
+        translations=[1033, 1252, 1251]
+    )
 
-# Build app
+# Build application
 PyInstaller.__main__.run([
     'main.spec'
 ])
+
+# Make .dmg
+if platform.system() == 'Darwin':
+    import dmgbuild
+
+    # build dmg
+    dmgbuild.build_dmg(
+        filename='dist/' + app_name + '.dmg',
+        volume_name=app_name,
+        settings={'files': ['dist/' + app_name + '.app']}
+    )
